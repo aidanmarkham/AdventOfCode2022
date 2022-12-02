e7c3bca713f69b7a01292cc2f2566b5af3829214
Aidan Markham <apm4337@g.rit.edu>
2022-12-01 23:39:27 -0500

    Day 1

---
+++ AdventOfCode2022/CalorieCounter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+	static class CalorieCounter
+	{
+
+		public static string inputFile = "calories.txt";
+
+		public static void CountCalories()
+		{
+			Console.WriteLine("Reading input file.");
+
+			string[] input = File.ReadAllLines(inputFile);
+
+			Console.WriteLine("Input file loaded! Calculating...");
+
+			List<int> elves = new List<int>();
+
+			elves.Add(0);
+
+			for(int i = 0; i < input.Length; i++)
+			{
+				// we're at an empty line, so we've finished an elf
+				if (input[i] == "")
+				{
+					elves.Add(0);
+				}
+				else
+				{
+					int lineValue = int.Parse(input[i]);
+
+					// add it to the last list element
+					elves[elves.Count - 1] += lineValue;
+				}
+			}
+
+			elves.Sort();
+
+			int lastThree = elves[elves.Count - 1] + elves[elves.Count - 2] + elves[elves.Count - 3];
+
+			Console.WriteLine("All files parsed correctly!");
+
+			Console.WriteLine("Max Calories: " + lastThree);
+		}
+
+	}
+
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("--- Welcome to Aidan's Advent of Code program. ---");
+			Console.WriteLine("");
+
+			// Day 1: Calorie Counter
+			Console.WriteLine("Day 1: Calorie Counter. Running Now!");
+			CalorieCounter.CountCalories();
+
+
+			Console.WriteLine("");
+			Console.WriteLine("Program complete! Press enter to quit.");
+			Console.ReadLine();
+		}
+	}
+}
