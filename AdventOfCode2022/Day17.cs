799a47da1dba18ffec123bf0ae4c349ae67e1fb3
Aidan Markham <apm4337@g.rit.edu>
2022-12-24 12:12:53 -0500

    day 17 pt 2

--- AdventOfCode2022/Day17.cs
+++ AdventOfCode2022/Day17.cs
@@ -11,16 +11,20 @@ namespace AdventOfCode2022
 {
 	class Day17
 	{
-		public static string inputFile = "tetris_test.txt";
+		public static string inputFile = "tetris.txt";
 		public static long rockCount = 1000000000000;
 
 		public static void Solve()
 		{
+			// a list to represent the board 
 			List<string> board = new List<string>();
+
+			// read in the directions as an array of chars
 			var directions = File.ReadAllText(inputFile);
-			int directionsIndex = 0;
+
+			// define shapes of tetris pieces
+			#region Piece Definitions
 			List<Tetronimo> pieces = new List<Tetronimo>();
-			int lcm = directions.Length * 5;
 			// create tetronimos
 			var pipe = new Tetronimo();
 			pipe.dimensions = (4, 1);
@@ -64,39 +68,87 @@ namespace AdventOfCode2022
 				new char[]{'#', '#'}
 			};
 			pieces.Add(box);
+			#endregion
 
+			// how many rocks left until we're done 
 			long remainingRocks = rockCount;
+
+			// the highest point so far 
 			long highestPoint = 0;
+
+			// loop iterator to determine what piece comes next 
 			int pieceIndex = 0;
+			// loop iterator to determine what direction comes next 
+			int directionsIndex = 0;
+
+			// how many steps does it take for both cycles to reset at the same time 
+			int lcm = directions.Length * pieces.Count;
+
+			// keep track of how many units of board we've trimmed off the bottom 
 			long boardOffset = 0;
-			Stopwatch watch = new Stopwatch();
-			watch.Start();
 
 			List<string> boards = new List<string>();
-			int lastLoopDex = -1;
-			int lastOffset = -1;
-			while (remainingRocks != 0)
-			{
-				long soFar = rockCount - remainingRocks;
 
-				if (remainingRocks % 10000 == 0)
+			long firstMatch = -1;
+			long lastMatch = 0;
+			long lastDist = -1;
+			long lastHeight = -1;
+			bool skipped = false;
+			long finalOffset = 0;
+			while (remainingRocks > 0)
+			{
+				// progress report 
+				long soFar = rockCount - remainingRocks;
+				long currentHeight = GetTowerHeight(board, boardOffset);
+				if (!skipped)
 				{
-					double rocksSoFar = rockCount - remainingRocks;
-					double progressScalar = rocksSoFar / (double)rockCount;
-					var ticksSoFar = watch.ElapsedTicks;
-					var time = new TimeSpan((long)(ticksSoFar / progressScalar));
-					Console.WriteLine("Highest Tetris: " + GetHighestTetris(board));
-					Console.WriteLine("Only " + remainingRocks + " left! Time remaining: " + time.ToString("h'h 'm'm 's's'"));
+					var boardString = OutputBoard(board, pieceIndex, directionsIndex);
+					if (boards.Contains(boardString))
+					{
+						if (firstMatch == -1)
+						{
+							firstMatch = currentHeight;
+						}
+						else
+						{
+							long rockDist = soFar - lastMatch;
+							long heightDist = currentHeight - lastHeight;
+
+							if (rockDist == lastDist)
+							{
+								skipped = true;
+								Console.WriteLine("Pattern found! Loop of " + heightDist + " rows happens starting after " + firstMatch + " rows. Rocks Contained: " + rockDist);
+
+								long numSkips = remainingRocks / rockDist;
+
+								long rockSkip = rockDist * numSkips;
+								long heightSkip = heightDist * numSkips;
+
+								Console.WriteLine("Skipping " + numSkips + " loops. " + rockDist + " rocks and " + heightDist + " rows");
+								remainingRocks -= rockSkip;
+								finalOffset += heightSkip;
+							}
+
+							lastMatch = soFar;
+							lastDist = rockDist;
+							lastHeight = currentHeight;
+						}
+						boards.Clear();
+					}
+					boards.Add(boardString);
 				}
 
 
 				// =============== spawn rock ====================
+				// calculate rock drop position
 				(long x, long y) rockPos = (2, highestPoint + 3);
 
+				// figure out what piece we need and select it 
 				if (pieceIndex == pieces.Count) pieceIndex = 0;
 				var piece = pieces[pieceIndex];
 				pieceIndex++;
 
+				// place it at the rock position 
 				piece.position = rockPos;
 
 				// make sure the board is tall enough 
@@ -105,43 +157,34 @@ namespace AdventOfCode2022
 					AddLines(board, 1);
 				}
 
-				bool obstructed = false;
+				// is the piece obstructed by something below it?
+				bool settled = false;
+
+				// how many steps have we fallen for (so we can know if we should move sideways or down)
 				int fallStepCount = 0;
 
-				// search for loops 
-				int loopDex = TrimBoard(board, ref boardOffset, lcm, ref boards, (int)soFar);
-				if (loopDex != -1)
+				//=========== drop rock ================
+				// while the current piece isn't settled 
+				while (!settled)
 				{
-					var offset = loopDex - lastLoopDex;
-					lastLoopDex = loopDex;
-					if(offset == lastOffset)
-					{
-						Console.WriteLine("Locked on to loop!");
-						while (remainingRocks > offset)
-						{
-							remainingRocks -= offset;
-							boardOffset += offset;
-						}
-					}
-					lastOffset = offset;
-				}
-
-				while (!obstructed)
-				{
-					var nextPos = piece.position;
+					// do we move sideways, or up and down?
 					bool sideways = fallStepCount % 2 == 0;
 					fallStepCount += 1;
+
+					// figure out what our next position will be 
+					var nextPos = piece.position;
+
+					// if we're moving sideways 
 					if (sideways)
 					{
+						// get a direction 
 						if (directionsIndex == directions.Length) directionsIndex = 0;
 						char direction = directions[directionsIndex];
 						directionsIndex++;
 
-						//Console.WriteLine("Pushing " + direction);
-
+						// offset position by the direction
 						if (direction == '<')
 						{
-
 							nextPos.x--;
 						}
 						else if (direction == '>')
@@ -150,11 +193,14 @@ namespace AdventOfCode2022
 						}
 						else
 						{
+							// bad data control
 							Console.WriteLine("Huh???");
 						}
 					}
+					// otherwise
 					else
 					{
+						// just move down
 						nextPos.y--;
 					}
 
@@ -166,65 +212,70 @@ namespace AdventOfCode2022
 					{
 						// don't make that move
 						nextPos = piece.position;
+
+						// if the move was going to be moving down 
 						if (!sideways)
 						{
-							obstructed = true;
+							// we're settled 
+							settled = true;
+
+							// imprint the piece on the board 
 							PrintPiece(board, piece, boardOffset);
 						}
 					}
 					else
 					{
+						// otherwise, loop through all the lines the piece is on 
 						for (int i = 0; i < piece.dimensions.y; i++)
 						{
+							// get that line from the board
 							var boardLine = board[(int)(nextPos.y + i - boardOffset)];
+
+							// get that line from the piece
 							var pieceLine = piece.GetLine(nextPos.y + i, nextPos);
 
+							// go through the lines
 							for (int j = 0; j < boardLine.Length; j++)
 							{
+								// if there's an intersection
 								if (boardLine[j] != ' ' && pieceLine[j] != ' ')
 								{
 									// don't make that move
 									nextPos = piece.position;
+
+									//if it was a downward move
 									if (!sideways)
 									{
-										obstructed = true;
+										// settle the piece
+										settled = true;
+
+										// and print it on the board 
 										PrintPiece(board, piece, boardOffset);
 									}
 								}
 							}
 						}
 					}
-					piece.position = nextPos;
 
+					// if we've made it this far, the piece isn't hitting anything yet, 
+					// so set it's position to the new position
+					piece.position = nextPos;
 				}
 
+				// now that we've got the new piece settled, keep track of the highest point on the tower 
 				highestPoint = GetTowerHeight(board, boardOffset);
 
+				// and decrement remaining rocks 
 				remainingRocks--;
 			}
 
-			DrawBoard(board, boardOffset);
-			Console.WriteLine("Tower Height: " + GetTowerHeight(board, boardOffset));
-		}
+			// at the end
+			// draw a picture of the board (or at least what hasn't been trimmed)
+			//DrawBoard(board, boardOffset);
 
-		private static int TrimBoard(List<string> board, ref long offset, int lcm, ref List<string> boards, int soFar)
-		{
-			int index = GetHighestTetris(board);
-			if (index != -1)
-			{
-				if (index > lcm)
-				{
-					board.RemoveRange(0, index + 1);
-					offset += index + 1;
-					AddLines(board, 1);
-
-					var boardString = OutputBoard(board, offset);
-					Console.WriteLine(boardString);
-					return soFar;
-				}
-			}
-
-			return -1;
+			// output the tower height
+			Console.WriteLine("Tower Height: " + GetTowerHeight(board, boardOffset) + " Final Offset: " + finalOffset);
+			Console.WriteLine("Final Height: " + (GetTowerHeight(board, boardOffset) + finalOffset));
 		}
 
 		private static int GetLowestTetris(List<string> board)
@@ -253,6 +304,7 @@ namespace AdventOfCode2022
 			return -1;
 		}
 
+		// go down the board starting from the top until we find blocks
 		private static long GetTowerHeight(List<string> board, long offset)
 		{
 			for (int i = board.Count - 1; i >= 0; i--)
@@ -262,6 +314,7 @@ namespace AdventOfCode2022
 			return offset;
 		}
 
+		// add some blank lines to the top of the board 
 		private static void AddLines(List<string> board, int quantity)
 		{
 			for (int i = 0; i < quantity; i++)
@@ -270,16 +323,7 @@ namespace AdventOfCode2022
 			}
 		}
 
-		private static void DrawBoard(List<string> board, Tetronimo piece, long offset)
-		{
-			for (int i = board.Count - 1; i >= 0; i--)
-			{
-				var lineString = piece.GetLine(offset + i, piece.position);
-				if (lineString.Length == 0) lineString = new string(board[i].ToArray());
-				Console.WriteLine((offset + i).ToString().PadLeft(5, '0') + "|" + lineString + "|");
-			}
-			Console.WriteLine((-1).ToString().PadLeft(5, '0') + "+-------+");
-		}
+		// print out the board in a human readable format
 		private static void DrawBoard(List<string> board, long offset)
 		{
 			for (int i = board.Count - 1; i >= 0; i--)
@@ -288,10 +332,13 @@ namespace AdventOfCode2022
 			}
 			Console.WriteLine((-1).ToString().PadLeft(5, '0') + "+-------+");
 		}
-		private static string OutputBoard(List<string> board, long offset)
+
+		// output the current board as a string for comparison 
+		private static string OutputBoard(List<string> board, int pieceIndex, int directionIndex)
 		{
-			var boardString = "";
-			for (int i = board.Count - 1; i >= 0; i--)
+			var boardString = pieceIndex + " " + directionIndex + "\n";
+			int lowerBound = Math.Max(0, board.Count - 10);
+			for (int i = board.Count - 1; i >= lowerBound; i--)
 			{
 				boardString += board[i] + "\n";
 			}
@@ -299,6 +346,7 @@ namespace AdventOfCode2022
 			return boardString;
 		}
 
+		// print the piece on the board in it's position 
 		private static void PrintPiece(List<string> board, Tetronimo piece, long offset)
 		{
 			for (int i = board.Count - 1; i >= 0; i--)
@@ -312,11 +360,11 @@ namespace AdventOfCode2022
 				{
 					if (pieceString[j] != ' ') board[i] = board[i].Remove(j, 1).Insert(j, pieceString[j].ToString());
 				}
-
-
 			}
 		}
 
+
+		// data class representing a tetronimo
 		public class Tetronimo
 		{
 			public (long x, long y) dimensions;
