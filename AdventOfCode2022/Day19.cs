6dc5e163c1d1c681018a4b3f49d1d7aa122a7f19
Aidan Markham <apm4337@g.rit.edu>
2022-12-24 10:24:48 -0500

    finished day 19

--- AdventOfCode2022/Day19.cs
+++ AdventOfCode2022/Day19.cs
@@ -9,14 +9,14 @@ namespace AdventOfCode2022
 {
 	class Day19
 	{
-		public static string inputFile = "robots_test.txt";
+		public static string inputFile = "robots.txt";
 
+		public static int totalTime = 32;
 		public static void Solve()
 		{
+			// read in input 
 			var input = File.ReadAllLines(inputFile);
-
 			List<Blueprint> blueprints = new List<Blueprint>();
-
 			for (int i = 0; i < input.Length; i++)
 			{
 				var words = input[i].Split(' ');
@@ -33,126 +33,168 @@ namespace AdventOfCode2022
 				blueprints.Add(bp);
 			}
 
-			int totalTime = 24;
-			State initialState = new State();
-			initialState.timeRemaining = totalTime;
-			initialState.oreBots = 1;
+			// total time for challenge
+			int totalQuality = 0;
 
-			Stack<State> states = new Stack<State>();
-			states.Push(initialState);
-			var blueprint = blueprints[0];
-			int mostGeo = 0;
-			State best = new State();
 
-			while (states.Count > 0)
+
+			for (int i = 0; i < 3; i++)
 			{
-				// the state to process
-				var state = states.Pop();
+				// set up initial state 
+				State initialState = new State();
+				initialState.timeRemaining = totalTime;
+				initialState.oreBots = 1;
 
-				// if this is a new best for geodes
-				if (state.geo > mostGeo)
+				// stack of states
+				Stack<State> states = new Stack<State>();
+				// add initial state
+				states.Push(initialState);
+
+				// grab the first blueprint for now
+				var blueprint = blueprints[i];
+
+				// used to save the best
+				int mostGeo = 0;
+				State best = new State();
+
+				long totalIterations = 0;
+
+				var maxOrePrice = Math.Max(Math.Max(blueprint.clayBotCost, blueprint.oreBotCost), Math.Max(blueprint.geoBotOreCost, blueprint.obsBotOreCost));
+				var maxClayPrice = blueprint.obsBotClayCost;
+				var maxObsPrice = blueprint.geoBotObsCost;
+
+				while (states.Count > 0)
 				{
-					mostGeo = state.geo;
-					best = state;
-					Console.WriteLine("Most Geo: " + mostGeo);
-				}
+					// the state to process
+					var state = states.Pop();
 
-				if((state.geoBots + state.timeRemaining) * state.timeRemaining < mostGeo)
-				{
-					continue;
-				}
+					// keep track of iterations
+					totalIterations++;
+					if (totalIterations % 10000000 == 0)
+					{
+						Console.WriteLine("Iterations: " + totalIterations);
+					}
 
-				// if we don't buy anything
-				State nextState = new State(state);
-				nextState.history += nextState;
-				nextState.history += "\nWaited.";
-				if (nextState.timeRemaining >= 0)
-				{
-					states.Push(nextState);
-				}
+					// if this is a new best for geodes
+					if (state.geo > mostGeo)
+					{
+						mostGeo = state.geo;
+						best = state;
+						Console.WriteLine("Most Geo: " + mostGeo);
+					}
 
-				// can afford ore bot
-				if (state.ore >= blueprint.oreBotCost)
-				{
-					nextState = new State(state);
-					// add an ore bot 
-					nextState.oreBots++;
-
-					// lower the ore count 
-					nextState.ore -= blueprint.oreBotCost;
-
-
-					nextState.history += nextState;
-					nextState.history += "\nBought ore bot.";
-
-					if (nextState.timeRemaining >= 0)
+					// if we don't buy anything
+					State nextState = new State(state);
+					if (ShouldIterate(nextState, mostGeo, blueprint))
 					{
 						states.Push(nextState);
 					}
-				}
 
-				// can afford clay bot 
-				if (state.ore >= blueprint.clayBotCost)
-				{
-					nextState = new State(state);
+					// calculate maximum possible needed ores
+					var maxNeededOre = Math.Max(Math.Max(blueprint.clayBotCost, blueprint.oreBotCost), Math.Max(blueprint.geoBotOreCost, blueprint.obsBotOreCost)) * state.timeRemaining;
+					var maxNeededClay = blueprint.obsBotClayCost * state.timeRemaining;
+					var maxNeededObs = blueprint.geoBotObsCost * state.timeRemaining;
 
-					// add a clay bot 
-					nextState.clayBots++;
 
-					// pay for it in ore
-					nextState.ore -= blueprint.clayBotCost;
 
-					nextState.history += nextState;
-					nextState.history += "\nBought clay bot.";
-					if (nextState.timeRemaining >= 0)
+					// if we can afford clay bot 
+					if (state.clayBots < maxClayPrice && state.clay < maxNeededClay && state.ore >= blueprint.clayBotCost)
 					{
-						states.Push(nextState);
+						nextState = new State(state);
+						// add a clay bot 
+						nextState.clayBots++;
+
+						// pay for it in ore
+						nextState.ore -= blueprint.clayBotCost;
+
+						if (ShouldIterate(nextState, mostGeo, blueprint))
+						{
+							states.Push(nextState);
+						}
+					}
+
+					// if we can afford ore bot
+					if (state.oreBots < maxOrePrice && state.ore < maxNeededOre && state.ore >= blueprint.oreBotCost)
+					{
+						nextState = new State(state);
+						// add an ore bot 
+						nextState.oreBots++;
+
+						// lower the ore count 
+						nextState.ore -= blueprint.oreBotCost;
+
+						if (ShouldIterate(nextState, mostGeo, blueprint))
+						{
+							states.Push(nextState);
+						}
+					}
+
+					// if we can afford obs bot
+					if (state.obsBots < maxObsPrice && state.obs < maxNeededObs && state.ore >= blueprint.obsBotOreCost && state.clay >= blueprint.obsBotClayCost)
+					{
+						nextState = new State(state);
+						// add an obs bot 
+						nextState.obsBots++;
+
+						// pay for it in ore and clay
+						nextState.ore -= blueprint.obsBotOreCost;
+						nextState.clay -= blueprint.obsBotClayCost;
+
+						if (ShouldIterate(nextState, mostGeo, blueprint))
+						{
+							states.Push(nextState);
+						}
+					}
+
+					// if we can afford geo bot
+					if (state.ore >= blueprint.geoBotOreCost && state.obs >= blueprint.geoBotObsCost)
+					{
+						nextState = new State(state);
+						// add a geo bot 
+						nextState.geoBots++;
+
+						// pay for it in ore and obsidian
+						nextState.ore -= blueprint.geoBotOreCost;
+						nextState.obs -= blueprint.geoBotObsCost;
+
+						if (ShouldIterate(nextState, mostGeo, blueprint))
+						{
+							states.Push(nextState);
+						}
 					}
 				}
 
-				// can afford obs bot
-				if (state.ore >= blueprint.obsBotOreCost && state.clay > blueprint.obsBotClayCost)
+				var thisQuality = best.geo;
+				Console.WriteLine("Blueprint " + (i+1) + " quality: " + thisQuality);
+				if(totalQuality == 0)
 				{
-					nextState = new State(state);
-
-					// add an obs bot 
-					nextState.obsBots++;
-
-					// pay for it in ore and clay
-					nextState.ore -= blueprint.obsBotOreCost;
-					nextState.clay -= blueprint.obsBotClayCost;
-
-					nextState.history += nextState;
-					nextState.history += "\nBought obs bot.";
-
-					if (nextState.timeRemaining >= 0)
-					{
-						states.Push(nextState);
-					}
+					totalQuality += thisQuality;
 				}
-
-				// can afford geo bot
-				if (state.ore >= blueprint.geoBotOreCost && state.obs > blueprint.geoBotObsCost)
+				else
 				{
-					nextState = new State(state);
-
-					// add a geo bot 
-					nextState.geoBots++;
-
-					// pay for it in ore and obsidian
-					nextState.ore -= blueprint.geoBotOreCost;
-					nextState.obs -= blueprint.geoBotObsCost;
-
-					nextState.history += nextState;
-					nextState.history += "\nBought geo bot.";
-					if (nextState.timeRemaining >= 0)
-					{
-						states.Push(nextState);
-					}
+					totalQuality *= thisQuality;
 				}
 			}
 
-			Console.WriteLine(best.history);
+			Console.WriteLine("Total Quality: " + totalQuality);
+		}
+
+		public static bool ShouldIterate(State state, int most, Blueprint bp)
+		{
+			var t = state.timeRemaining;
+			bool time = t >= 0;
+
+			// what we currently have
+			var possibleGeo = state.geo;
+
+			// what we could make with the bots we have
+			possibleGeo += state.geoBots * t;
+
+			// what we could possibly build if we had a new bot each round
+			possibleGeo += (t * (t - 1)) / 2;
+
+			bool botSprint = possibleGeo > most;
+			return time && botSprint;
 		}
 
 		public struct State
@@ -166,7 +208,6 @@ namespace AdventOfCode2022
 			public int clay;
 			public int obs;
 			public int geo;
-			public string history;
 			public State(State state) : this()
 			{
 				timeRemaining = state.timeRemaining - 1;
@@ -175,23 +216,11 @@ namespace AdventOfCode2022
 				clayBots = state.clayBots;
 				obsBots = state.obsBots;
 				geoBots = state.geoBots;
+
 				ore = state.ore + state.oreBots;
 				clay = state.clay + state.clayBots;
 				obs = state.obs + state.obsBots;
 				geo = state.geo + state.geoBots;
-				history = state.history;
-			}
-
-			public override string ToString()
-			{
-				return "";
-				string history = "";
-				history += "\n=== Minute " + (24 - timeRemaining) + " ===";
-				history += "\n Ore: " + ore;
-				history += "\n Clay: " + clay;
-				history += "\n Obsidian: " + obs;
-				history += "\n Geodes: " + geo;
-				return history;
 			}
 		}
 
@@ -209,6 +238,5 @@ namespace AdventOfCode2022
 				return " " + oreBotCost + clayBotCost + obsBotOreCost + obsBotClayCost + geoBotOreCost + geoBotObsCost;
 			}
 		}
-
 	}
 }
